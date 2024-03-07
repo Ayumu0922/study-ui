26c0b0d9d23585419e75ac3530fa526a03654ece
Ayumu0922 <9bota.ayu6@yahoo.ne.jp>
2024-03-07 16:40:31 +0900

    create datepicker

--- src/app/dashboard/patients/page.tsx
+++ src/app/dashboard/patients/page.tsx
@@ -1,17 +1,30 @@
 "use client";
 import SearchBar from "@/components/SearchBar/SearchBar";
 import SelectForm from "@/components/SelectForm/SelectForm";
-import React from "react";
+import React, { useState } from "react";
 import { FaUsers } from "react-icons/fa";
+import Datepicker from "react-tailwindcss-datepicker";
 
 const PatientPage = () => {
   const options = [
     { value: "All", label: "All" },
     { value: "Today", label: "Today" },
+    { value: "Tommorrow", label: "Tommorrow" },
+    { value: "Last Week", label: "Last Week" },
   ];
 
+  // date picker の状態を any 型で定義
+  const [value, setValue] = useState<any>({
+    startDate: new Date(),
+    endDate: new Date(new Date().setMonth(new Date().getMonth() + 1)),
+  });
+
+  const handleValueChange = (newValue: any, e?: HTMLInputElement) => {
+    console.log("newValue:", newValue);
+    setValue(newValue);
+  };
+
   return (
-    // 高さが画面いっぱいになるように調整しました。
     <div className="w-full h-screen  bg-gray-100 flex flex-col z-0">
       {/* top bar */}
       <div className="bg-indigo-400 text-white h-12 flex items-center px-4 font-bold text-md gap-[35%] ">
@@ -29,6 +42,7 @@ const PatientPage = () => {
             options={options}
             defaultValue="Choose a date"
           />
+          <Datepicker value={value} onChange={handleValueChange} />
         </div>
         {/* right content */}
         <div className="flex-1 overflow-y-auto">
--- src/components/SelectForm/SelectForm.tsx
+++ src/components/SelectForm/SelectForm.tsx
@@ -20,13 +20,13 @@ const SelectForm: React.FC<SelectFormProps> = ({
     <form className="max-w-sm mx-auto">
       <label
         htmlFor="select"
-        className="block mb-2 text-sm font-medium text-gray-900"
+        className="block  m-1 text-md font-medium text-gray-900 "
       >
         {label}
       </label>
       <select
         id="select"
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5 focus:outline-none"
         defaultValue={defaultValue}
       >
         <option disabled>{defaultValue}</option>
